b7c36f59c6bdfe921d3c8d2201b6d73cba66b0a6
KLcpb <138727426+KLcpb@users.noreply.github.com>
2024-07-04 22:31:48 +0300

    add plotting altitude from pressure

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from source import drawer
+import os
 
 ##CREATE FOLDERS AUTOMATICALLY
-PATH = './log2.txt'
+PATH = './telem.TXT'
 SETTING_PATH = './config.txt'
 OUTPUT_FOLDER = './OUTPUT'
+
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+
+
 Drawer = drawer(OUTPUT_FOLDER)
-params = Drawer.load_params(SETTING_PATH)
+params = Drawer.load_params()
 print(f'loaded settings from {SETTING_PATH}')
 print(params)
-Drawer.load_file(PATH)
+val = Drawer.load_file()
+print(f'found {val} strs')
 print(f'loaded file from {PATH}')
 Drawer.process_raw()
 
--- source.py
+++ source.py
@@ -10,16 +10,30 @@ class drawer():
     def __draw(self,param_name,path):
         if param_name == "TIME" or param_name == "NUMBER":
             return
+        data_to_plot = self.data[param_name]
+        param_to_plot_name = param_name
+        if param_name == "ALTITUDE":
+            try:
+                data_to_plot = self.__process_altitude_from_pressure(self.data["PRESS"])
+            except KeyError:
+                raise NameError(f'Invalid params, if ALTITUDE is drawed its necessary to have PRESS in params')
+            param_to_plot_name = "ALTITUDE WITH BIAS"
+
         
         plt.gcf().set_size_inches(18, 10)
-        plt.locator_params(axis='both', nbins=10) 
-        plt.ylabel(param_name)
-        plt.plot(self.data[param_name])
+        plt.locator_params(axis='both', nbins=10)
+        plt.ylabel(param_to_plot_name)
+        plt.plot(data_to_plot)
         plt.savefig(path)
         plt.cla()
         plt.clf()
+    def __process_altitude_from_pressure(self,press):
+        ALT_BIAS = 150
+        print(f'! altitude bias set to {ALT_BIAS}')
+        
+        return list(map(lambda x: float(44330*(1-(x/101300)**0.1903) - ALT_BIAS),press))
 
-    def load_params(self,path):
+    def load_params(self,path='./config.txt'):
         with open(path,'r') as file:
             #get params from config file
             for string in file.readlines():
@@ -33,24 +47,29 @@ class drawer():
 
     def __set_params(self,string):
         self.params = string.split(';')
+        self.params[-1]=self.params[-1][:-1]
         for param in self.params:
             self.data[param] = []
-        
-    def load_file(self,path):
+
+    def load_file(self):
+        path = self.settings["logpath"][1:-2]
         with open(path,'r') as file:
             ##add values from file to special variable
+            cnt=0
             for string in file.readlines():
+                cnt+=1
                 vals = string.split(';')
                 try:
                     for i in range(len(vals)):
                         self.data[self.params[i]].append(float(vals[i]))
                 except IndexError:
                     print("INVALID CONFIG FILE")
-                    return
-    
+                    return -1
+            return cnt
+            
     def process_raw(self):
         for param in self.params:
-            self.__draw(param,self.output_folder + '/RAW/' + param + '.png')
-
+            self.__draw(param, "./" + self.output_folder+"/" + param + '.png')
+       
     def __detect_start(self):
         pass
